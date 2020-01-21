9cce995f4acda8c4df819918e383b4d0770f310e
Nicolas BAUW <nbauw@hotmail.com>
2020-01-21 16:58:04 +0100

    Area clear function

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,14 @@ pub fn blit(source: &Vec<u32>, destination: &mut Vec<u32>, screen_width: usize, 
     }
 }
 
+pub fn clear_area(buffer: &mut Vec<u32>, screen_width: usize, w: usize, h: usize, x: usize, y: usize, clear_color: u32) {
+    for inc_y in 0..h {
+        for inc_x in 0..w {
+            buffer[inc_x + inc_y*screen_width + x + y*screen_width] = clear_color;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
