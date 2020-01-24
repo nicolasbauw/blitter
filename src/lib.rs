f8d38102aeccde065d9a22c7065eeaade55ff043
Nicolas BAUW <nbauw@hotmail.com>
2020-01-24 21:48:37 +0100

    Functions are now methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-/// This structure's persists user bitmaps sizes, coordinates, and stores a pointer to its pixel data
+/// This structure's contains bitmaps sizes, coordinates, and a pointer to its pixel data
 pub struct Bitmap<'a> {
     /// Bitmap width
     pub w: usize,
@@ -9,42 +9,53 @@ pub struct Bitmap<'a> {
     /// Bitmap vertical position
     pub y: usize,
     /// Pixel data
-    pub pixels: &'a Vec<u32>
+    pub pixels: &'a Vec<u32>,
 }
 
 /// The framebuffer struct contains the buffer's width, height, and a pointer to its pixel data
 pub struct Framebuffer<'a> {
     pub width: usize,
     pub height: usize,
-    pub pixels: &'a mut Vec<u32>
+    pub pixels: &'a mut Vec<u32>,
 }
 
-/// Copies a Bitmap to the framebuffer
-pub fn blit(bitmap: &Bitmap, fb: &mut Framebuffer) {
-    for inc_y in 0..bitmap.h {
-        let x_offset: usize = inc_y*fb.width;
-        let y_offset: usize = bitmap.y*fb.width;
-        for inc_x in 0..bitmap.w {
-            fb.pixels[inc_x + x_offset + bitmap.x + y_offset] = bitmap.pixels[inc_x];
+impl Bitmap<'_> {
+    /// Copies a Bitmap to the framebuffer
+    pub fn blit(&self, fb: &mut Framebuffer) {
+        for inc_y in 0..self.h {
+            let x_offset: usize = inc_y * fb.width;
+            let y_offset: usize = self.y * fb.width;
+            for inc_x in 0..self.w {
+                fb.pixels[inc_x + x_offset + self.x + y_offset] = self.pixels[inc_x];
+            }
         }
     }
 }
 
-/// Partial clear of the framebuffer
-pub fn clear_area(fb: &mut Framebuffer, w: usize, h: usize, x: usize, y: usize, clear_color: u32) {
-    for inc_y in 0..h {
-        let x_offset: usize = inc_y*fb.width;
-        let y_offset: usize = y*fb.width;
-        for inc_x in 0..w {
-            fb.pixels[inc_x + x_offset + x + y_offset] = clear_color;
+impl Framebuffer<'_> {
+    /// Partial clear of the framebuffer
+    pub fn clear_area(
+        &mut self,
+        w: usize,
+        h: usize,
+        x: usize,
+        y: usize,
+        clear_color: u32,
+    ) {
+        for inc_y in 0..h {
+            let x_offset: usize = inc_y * self.width;
+            let y_offset: usize = y * self.width;
+            for inc_x in 0..w {
+                self.pixels[inc_x + x_offset + x + y_offset] = clear_color;
+            }
         }
     }
-}
 
-/// Complete clear of the framebuffer
-pub fn clear_buffer(fb: &mut Framebuffer, clear_color: u32) {
-    for inc_x in 0..fb.width*fb.height {
-        fb.pixels[inc_x] = clear_color;
+    /// Complete clear of the framebuffer
+    pub fn clear(&mut self, clear_color: u32) {
+        for inc_x in 0..self.width * self.height {
+            self.pixels[inc_x] = clear_color;
+        }
     }
 }
 
