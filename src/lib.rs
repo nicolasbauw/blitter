0e5c6321cae6e24d34ba1a7813459837c0c1833d
Nicolas BAUW <nbauw@hotmail.com>
2020-01-24 14:12:29 +0100

    Some comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
+/// This structure's goal is to persist user bitmaps sizes and coordinates
 pub struct BmpBlt {
+    /// Framebuffer width
     pub screen_width: usize,
+    /// Bitmap width
     pub w: usize,
+    /// Bitmap height
     pub h: usize,
+    /// Bitmap horizontal position
     pub x: usize,
+    /// Bitmap vertical position
     pub y: usize
 }
 
+/// Copies a bitmap to the framebuffer
 pub fn blit(source: &Vec<u32>, destination: &mut Vec<u32>, bmp: &BmpBlt) {
     for inc_y in 0..bmp.h {
         let x_offset: usize = inc_y*bmp.screen_width;
@@ -16,6 +23,7 @@ pub fn blit(source: &Vec<u32>, destination: &mut Vec<u32>, bmp: &BmpBlt) {
     }
 }
 
+/// Partial clear a the framebuffer
 pub fn clear_area(buffer: &mut Vec<u32>, screen_width: usize, w: usize, h: usize, x: usize, y: usize, clear_color: u32) {
     for inc_y in 0..h {
         let x_offset: usize = inc_y*screen_width;
@@ -26,6 +34,7 @@ pub fn clear_area(buffer: &mut Vec<u32>, screen_width: usize, w: usize, h: usize
     }
 }
 
+/// Complete clear of the framebuffer
 pub fn clear_buffer(buffer: &mut Vec<u32>, screen_width: usize, screen_height: usize, clear_color: u32) {
     for inc_x in 0..screen_width*screen_height {
         buffer[inc_x] = clear_color;
