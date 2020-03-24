45b4230a2d714dfe11de8695df09f7eedf353bc7
Nicolas BAUW <nbauw@hotmail.com>
2020-03-24 12:11:42 +0100

    Mask test + fix

--- examples/minifb.rs
+++ examples/minifb.rs
@@ -13,6 +13,7 @@ fn main() {
     // User bitmaps generating / loading
     let image:  Vec<u32> =  { vec![0xffffffff; 100] };
     let image2:  Vec<u32> =  { vec![0x0000ff00; 2500] };
+    let image3:  Vec<u32> =  { vec![0x0000ff00; 100] };
     let path = "resources/test-image.png";
     let png = from_png_file(&path, PixelFormat::Zrgb).unwrap();
 
@@ -21,6 +22,7 @@ fn main() {
     bitmaps.push(Bitmap {w: 10, h: 10, x: 0, y: 0, pixels: &image});
     bitmaps.push(Bitmap{w: png.0, h: png.1, x: 0, y: 320, pixels: &png.2});
     bitmaps.push(Bitmap {w: 50, h: 50, x: 590, y: 470, pixels: &image2});
+    bitmaps.push(Bitmap {w: 10, h: 10, x: 630, y: 460, pixels: &image3});
 
     let mut window = Window::new(
         "Test - ESC to exit",
@@ -49,6 +51,16 @@ fn main() {
 
 // For testing : moves a 10x10 square and prints a 4x4 pixel at the center of the screen
 fn blitter_test(mut fb: &mut Framebuffer, bitmaps: &mut Vec<Bitmap>) {
+    let test_mask = vec![true, false, true, false, true, false, true, false, true, false,
+    false, true, false, true, false, true, false, true, false, true,
+    true, false, true, false, true, false, true, false, true, false,
+    false, true, false, true, false, true, false, true, false, true,
+    true, false, true, false, true, false, true, false, true, false,
+    false, true, false, true, false, true, false, true, false, true,
+    true, false, true, false, true, false, true, false, true, false,
+    false, true, false, true, false, true, false, true, false, true,
+    true, false, true, false, true, false, true, false, true, false,
+    false, true, false, true, false, true, false, true, false, true];
     // We just clear the animated part of the screen
     fb.clear_area(640, 10, 0, 0, 0).unwrap();
     bitmaps[0].blit(&mut fb);
@@ -57,4 +69,6 @@ fn blitter_test(mut fb: &mut Framebuffer, bitmaps: &mut Vec<Bitmap>) {
     bitmaps[2].blit(&mut fb);
     if bitmaps[0].x < WIDTH as isize - 10 { bitmaps[0].x = bitmaps[0].x+3; }
     fb.draw_fatpixel(WIDTH/2, HEIGHT/2, 4, 0xffffffff).unwrap();
+    let mask = Mask::Bits(&test_mask);
+    bitmaps[3].blit_mask(&mut fb, mask);
 }
